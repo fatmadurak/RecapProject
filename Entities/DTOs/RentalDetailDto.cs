d0b924e14b8bb2b9e4978a528fa19a7033757606
Fatma DURAK <86356645+fatmadurak@users.noreply.github.com>
2021-08-16 14:48:49 +0300

    Add files via upload

---
+++ Entities/DTOs/RentalDetailDto.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+   public class RentalDetailDto:IDto
+    {
+
+        public int CarId { get; set; }
+
+        public DateTime RentDate { get; set; }
+
+        public DateTime ReturnDate { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
